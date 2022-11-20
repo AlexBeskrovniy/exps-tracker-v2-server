c6d665a25f3981d7f9b1226b2ab13874867756a5
AlexBlacksmith <unbloody.alex@gmail.com>
2022-11-20 11:48:42 +0500

    Removed all old files from repo

--- models/total.mjs
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const totalSchema = new mongoose.Schema({
-    name: { type: String, default: "Total"},
-    totalSpent: { type: Number, default: 0 }
-}, { timestamps: true });
-
-export const Total = mongoose.model('Total', totalSchema, 'totals');
